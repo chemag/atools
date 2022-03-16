afc78ef80cc5989bffe928686ee8a561f5585f83
Chema Gonzalez <chemag@fb.com>
2022-03-16 16:40:44 -0700

    atools: lint findecho.py

--- python/findecho.py
+++ python/findecho.py
@@ -13,24 +13,26 @@ import soundfile as sf
 
 
 def main():
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument("-i", "--impulse",required=True)
-    parser.add_argument("-o", "--output_csv", required=True)
-    parser.add_argument("-t", "--seconds", required=False, default=30, type=int)
-    parser.add_argument("-d", "--delay", required=False, default=2, type=int)
-    parser.add_argument("-s", "--save", required=False, default="")
-    parser.add_argument("-v", "--verbose", required=False, action="store_true")
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-i', '--impulse', required=True)
+    parser.add_argument('-o', '--output_csv', required=True)
+    parser.add_argument('-t', '--seconds', required=False, default=30,
+                        type=int)
+    parser.add_argument('-d', '--delay', required=False, default=2, type=int)
+    parser.add_argument('-s', '--save', required=False, default='')
+    parser.add_argument('-v', '--verbose', required=False,
+                        action='store_true')
     options = parser.parse_args()
     global running
     global index
 
     start = time.time()
     now = start
-    if options.impulse[-3:] == "wav":
-        ref = sf.SoundFile(options.impulse, "r")
+    if options.impulse[-3:] == 'wav':
+        ref = sf.SoundFile(options.impulse, 'r')
         ref_data = ref.read()
     else:
-        print("Only wav file supported")
+        print('Only wav file supported')
         exit(0)
 
     outputfile = None
@@ -39,25 +41,26 @@ def main():
     delays = []
 
     while now - start < options.seconds:
-        pr.play_and_record(options.impulse, options.output_csv + "_.wav", options.delay)
-        noisy = sf.SoundFile(options.output_csv + "_.wav", "r")
+        pr.play_and_record(options.impulse, options.output_csv + '_.wav',
+                           options.delay)
+        noisy = sf.SoundFile(options.output_csv + '_.wav', 'r')
         noisy_data = noisy.read()
         if (len(options.save) > 0) & (outputfile is None):
             outputfile = sf.SoundFile(
-                options.save, "w", samplerate=noisy.samplerate, channels=noisy.channels
-            )
+                options.save, 'w', samplerate=noisy.samplerate,
+                channels=noisy.channels)
 
         data = cm.find_markers(
-            ref_data, noisy_data, threshold, noisy.samplerate, verbose=options.verbose
-        )
+            ref_data, noisy_data, threshold, noisy.samplerate,
+            verbose=options.verbose)
         if len(data) > 1:
-            diff = data.loc[1, "sample"] - data.loc[0, "sample"]
+            diff = data.loc[1, 'sample'] - data.loc[0, 'sample']
             delays.append(
                 [
                     diff,
                     round(diff / noisy.samplerate * 1000),
-                    data.loc[0, "correlation"],
-                    data.loc[1, "correlation"],
+                    data.loc[0, 'correlation'],
+                    data.loc[1, 'correlation'],
                 ]
             )
             if outputfile:
@@ -70,36 +73,38 @@ def main():
         if perc <= 100:
             if failed_counter > 0:
                 total = len(delays) + failed_counter
-                print(f"{perc}% ({int(100 * failed_counter/(total))}% failures)")
+                print(f'{perc}% ({int(100 * failed_counter/(total))}%'
+                      ' failures)')
             else:
-                print(f"{perc}%")
+                print(f'{perc}%')
 
     if outputfile:
         outputfile.close()
-    labels = ["samples", "time", "correlation1", "correlation2"]
-    result = pd.DataFrame.from_records(delays, columns=labels, coerce_float=True)
+    labels = ['samples', 'time', 'correlation1', 'correlation2']
+    result = pd.DataFrame.from_records(delays, columns=labels,
+                                       coerce_float=True)
 
     output_filename = options.output_csv
-    if (options.output_csv is not None) & (options.output_csv[-4:] != ".csv"):
-        output_filename = options.output_csv + ".csv"
+    if (options.output_csv is not None) & (options.output_csv[-4:] != '.csv'):
+        output_filename = options.output_csv + '.csv'
     if options.output_csv:
         result.to_csv(output_filename, index=False)
     if failed_counter > 0:
         total = len(delays) + failed_counter
         print(
-            f"Failed {failed_counter} tries out of {total} ({int(100 * failed_counter/(total))})%"
-        )
+            f'Failed {failed_counter} tries out of {total} '
+            f'({int(100 * failed_counter/(total))})%')
         print(
-            "If this percentage is high it is an indicator on problems with the transmission."
-        )
+            'If this percentage is high it is an indicator on problems '
+            'with the transmission.')
 
     if len(result > 0):
-        print(f"Shortest latency: {int(np.min(result['time']))} ms")
-        print(f"Longest latency: {int(np.max(result['time']))} ms")
-        print(f"Average latency: {int(np.mean(result['time']))} ms")
+        print(f'Shortest latency: {int(np.min(result["time"]))} ms')
+        print(f'Longest latency: {int(np.max(result["time"]))} ms')
+        print(f'Average latency: {int(np.mean(result["time"]))} ms')
 
-    os.remove(options.output_csv + "_.wav")
+    os.remove(options.output_csv + '_.wav')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
