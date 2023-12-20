f9672f8481e0818d71544c644d2aadb591b98084
Chema Gonzalez <chemag@meta.com>
2023-12-20 08:38:56 -0800

    atools: analyze: add generic audio analyzer
    First analysis is frequency dump.
    
    Tested:
    ```
    $ ./python/aanalyze.py --analysis frequency -i /tmp/white.wav -o /tmp/white.wav.csv
    $ csvlook -I /tmp/white.wav.csv
    | frequency          | power              |
    | ------------------ | ------------------ |
    | 0.0                | 910042.0           |
    | 1.0                | 1080479.1777541237 |
    | 2.0                | 2020401.3468236814 |
    | 3.0                | 627068.1925977293  |
    | 4.0                | 483566.72542686114 |
    | 5.0                | 2185307.3646881757 |
    | 6.0                | 929051.3573328258  |
    | 7.0                | 577014.8498551622  |
    ...
    | 7991.000000000001  | 1293220.8494019809 |
    | 7992.0             | 1966470.3102018603 |
    | 7993.0             | 2187745.854470799  |
    | 7994.0             | 1511099.0937721522 |
    | 7995.0             | 1713828.7449440523 |
    | 7996.0             | 616073.3440551193  |
    | 7997.0             | 651730.0203754746  |
    | 7998.0             | 1621137.3142985066 |
    | 7999.000000000001  | 1473562.9317467674 |
    | 8000.0             | 833484.0           |
    ```

---
+++ python/aanalyze.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+"""analyze.py module description.
+
+Runs a generic audio analyzer.
+"""
+
+
+import argparse
+import math
+import numpy as np
+import os
+import pandas as pd
+import sys
+import scipy.io.wavfile
+import scipy.signal
+
+
+ANALYSIS_CHOICES = ("frequency",)
+
+default_values = {
+    "debug": 0,
+    "dry_run": False,
+    "analysis": "frequency",
+    "infile": None,
+    "outfile": None,
+}
+
+
+# generic frequency analyzer
+# https://stackoverflow.com/a/67127726
+def analyze_frequencies(infile):
+    # read the input
+    samplerate, inaud = scipy.io.wavfile.read(infile)
+    # calculate the Discrete Fourier Transform sample frequencies
+    freq_list = np.fft.rfftfreq(len(inaud)) * samplerate
+    power_values = np.abs(np.fft.rfft(inaud))
+    freq_df = pd.DataFrame(
+        columns=["frequency", "power"], data=zip(freq_list, power_values)
+    )
+    return freq_df
+
+
+def run_audio_analyze(options):
+    # process the input
+    if options.analysis == "frequency":
+        freq_df = analyze_frequencies(options.infile)
+        freq_df.to_csv(options.outfile, index=False)
+
+
+def get_options(argv):
+    """Generic option parser.
+
+    Args:
+        argv: list containing arguments
+
+    Returns:
+        Namespace - An argparse.ArgumentParser-analyzed option object
+    """
+    # init parser
+    # usage = 'usage: %prog [options] arg1 arg2'
+    # parser = argparse.OptionParser(usage=usage)
+    # parser.print_help() to get argparse.usage (large help)
+    # parser.print_usage() to get argparse.usage (just usage line)
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="store_true",
+        dest="version",
+        default=False,
+        help="Print version",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="count",
+        dest="debug",
+        default=default_values["debug"],
+        help="Increase verbosity (use multiple times for more)",
+    )
+    parser.add_argument(
+        "--quiet",
+        action="store_const",
+        dest="debug",
+        const=-1,
+        help="Zero verbosity",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        dest="dry_run",
+        default=default_values["dry_run"],
+        help="Dry run",
+    )
+    parser.add_argument(
+        "--analysis",
+        action="store",
+        type=str,
+        dest="analysis",
+        default=default_values["analysis"],
+        choices=ANALYSIS_CHOICES,
+        metavar="[%s]"
+        % (
+            " | ".join(
+                ANALYSIS_CHOICES,
+            )
+        ),
+        help="analysis arg",
+    )
+    parser.add_argument(
+        "-i",
+        "--infile",
+        dest="infile",
+        type=str,
+        default=default_values["infile"],
+        metavar="input-file",
+        help="input file",
+    )
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        dest="outfile",
+        type=str,
+        default=default_values["outfile"],
+        metavar="output-file",
+        help="output file",
+    )
+    # do the parsing
+    options = parser.parse_args(argv[1:])
+    if options.version:
+        return options
+    return options
+
+
+def main(argv):
+    # parse options
+    options = get_options(argv)
+    # get infile/outfile
+    if options.infile is None or options.infile == "-":
+        options.infile = "/dev/fd/0"
+    if options.outfile is None or options.outfile == "-":
+        options.outfile = "/dev/fd/1"
+    # print results
+    if options.debug > 0:
+        print(options)
+
+    run_audio_analyze(options)
+
+
+if __name__ == "__main__":
+    # at least the CLI program name: (CLI) execution
+    main(sys.argv)
